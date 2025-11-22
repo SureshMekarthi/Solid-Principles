896e6c405be0aeb8f1c6b5015de9a5c052f22d12
Suresh Mekarthi <sureshmekarthi@gmail.com>
2025-11-22 13:24:13 -0500

    Add project files.

---
+++ Solid Principles/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles
+{
+    internal class UserService
+    {
+        //public bool isValidEmail(string email)
+        //{
+        //  return email.Contains("@");
+        //}
+
+        public void RegisterUser(string email)
+        {
+            if (!email.Contains("@"))
+            {
+                throw new Exception("Invalid email address");
+            }
+
+            // Registration logic here
+
+            Console.WriteLine("User registered successfully.");
+            Console.WriteLine("Sending welcome email to " + email);
+        }
+    }
+}
