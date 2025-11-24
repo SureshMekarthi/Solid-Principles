3bb73db5d83dd9fb766b8dbd58545057d8baa848
Suresh Mekarthi <sureshmekarthi@gmail.com>
2025-11-23 19:20:09 -0500

    voilation of OCP

---
+++ Solid Principles/PaymentService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles
+{
+    internal class PaymentService
+    {
+        public void ProcessPayment(string paymentMethod, decimal amount)
+        {
+            if (paymentMethod == "CreditCard")
+            {
+                ProcessCreditCardPayment(amount);
+            }
+            else if (paymentMethod == "PayPal")
+            {
+                ProcessPayPalPayment(amount);
+            }
+            else
+            {
+                throw new Exception("Unsupported payment method");
+            }
+        }
+        private void ProcessCreditCardPayment(decimal amount)
+        {
+            Console.WriteLine("Processing credit card payment of " + amount);
+        }
+        private void ProcessPayPalPayment(decimal amount)
+        {
+            Console.WriteLine("Processing PayPal payment of " + amount);
+        }
+    }
+}
